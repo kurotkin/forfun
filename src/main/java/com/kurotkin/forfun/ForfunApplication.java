4e3f49e8037a99c63196a0428ba89ef729814fc0
kurotkin <kurotkin@users.noreply.github.com>
2019-10-01 21:05:43 +0300

    Тестовый REST

---
+++ src/main/java/com/kurotkin/forfun/ForfunApplication.java
@@ -0,0 +1,13 @@
+package com.kurotkin.forfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForfunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForfunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kurotkin/forfun/ForfunApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kurotkin.forfun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ForfunApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
